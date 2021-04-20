78e463804bdae311b4b1338cf40378b66ce897cc
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-20 10:35:20 -0300

    add this components ever visible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
 
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  );
+}
 export default MyApp
