347995e059f1fb9e1a920ca4e2edbf81f7af59df
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-20 09:38:39 -0300

    language switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export function Header(){
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr"/>
 
-            <p>O melhor para você ouvir, sempre</p>
+            <p>The best for you to hear, always</p>
 
             <span>{currentDate}</span>
         </header>
