cdecc39d282467fdb1411fdeb792f0f61cacd3d9
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-22 23:44:38 -0300

    add new component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,17 @@
+import { useContext } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
+import { PlayerContext } from '../../contexts/PlayerContext';
+import Image from 'next/image';
 import styles from  './styles.module.scss';
 
 export function Player(){
+    const {episodeList, currentEpisodeIndex} = useContext(PlayerContext);
+
+    const episode = episodeList[currentEpisodeIndex];
+
+
     return (
         <div className={styles.playerContainer}>
             <header>
@@ -8,15 +19,34 @@ export function Player(){
                 <strong>Playing now</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Select a podcast to listen</strong>
-            </div>
+            {episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image 
+                        width={592} 
+                        height={592} 
+                        src={episode.thumbnail} 
+                        objectFit="cover"
+                    />     
+                    <strong>{episode.title}</strong>      
+                    <strong>{episode.members}</strong>       
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Select a podcast to listen</strong>
+                </div>
+            )}
+            
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider}/>
+                        { episode ?
+                        (
+                            <Slider/>
+                        ): (
+                            <div className={styles.emptySlider}/>
+                        )}
                     </div>
                     <span>00:00</span>
                 </div>
