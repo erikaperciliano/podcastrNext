19cbbb1a83a8fb048173bd3e9079b003e3b2e0ef
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-22 21:36:49 -0300

    add routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,6 @@ type Episode = {
   publishedAt: string;
   duration: number;
   durationAsString: string;
-  description: string;
   url: string;
 }
 
@@ -67,11 +66,13 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
 
           <table cellSpacing={0}>
             <thead>
-              <th>Podcast</th>
-              <th>Members</th>
-              <th>Date</th>
-              <th>Duration</th>
-              <th></th>
+              <tr>
+                <th>Podcast</th>
+                <th>Members</th>
+                <th>Date</th>
+                <th>Duration</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
               {allEpisodes.map(episode => {
@@ -87,7 +88,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                       />
                     </td>
                     <td>
-                      <a href="">{episode.title}</a>
+                      <Link  href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                      </Link>
                     </td>
                     <td>{episode.members}</td>
                     <td style={{width: 100}}>{episode.publishedAt}</td>
@@ -126,7 +129,6 @@ export  const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url
     };
   });
