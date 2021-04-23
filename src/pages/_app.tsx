14fd3baa9a7f37394c14bbc442caf4e0f58c8584
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-04-22 23:45:16 -0300

    call context api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,32 @@ import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
+import { useState } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setEpisodeIndex] = useState(0);
+
+  function play(episode){
+    setEpisodeList([episode]);
+    setEpisodeIndex(0);
+  }
+
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header/>
-        <Component {...pageProps} />
-      </main>
-      <Player/>
-    </div>
+    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play}}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header/>
+          <Component {...pageProps} />
+        </main>
+        <Player/>
+      </div>
+    </PlayerContext.Provider>
   );
 }
 export default MyApp
